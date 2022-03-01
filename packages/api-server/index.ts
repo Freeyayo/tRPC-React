a1660a40f489f4651834ecf654d405c40291fcf0
caiconghao <caiconghao@flyfinger.com>
2022-03-02 01:07:37 +0800

    setup client

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,10 +1,30 @@
 import express from "express";
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
+
+const appRouter = trpc.router().query("hello", {
+  resolve() {
+    return "hello world";
+  },
+});
+
+export type AppRouter = typeof appRouter;
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("Hello from api-server");
+  res.send("Hello from api-ser/*  */ver");
 });
 
 app.listen(port, () => {
